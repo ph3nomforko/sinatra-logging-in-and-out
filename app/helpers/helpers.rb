7856f296ad4d6b2932bcc9e995bf415f73404d8a
Codi <codi.kraus@gmail.com>
2021-07-26 11:33:10 -0500

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,10 @@
 class Helpers
     def self.current_user(session_hash)
-        User.find(session_hash["user_id"])
+        @user = User.find_by_id(session_hash[:user_id])
+        @user
     end
 
     def self.is_logged_in?(session_hash)
-        !!session_hash["user_id"]
+        !!session_hash[:user_id]
     end
 end
